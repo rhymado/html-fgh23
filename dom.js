1bcb8336b4787d689eb4ed652148045f5cbf4fed
rhymaado <mightydhiyamatra@yahoo.co.id>
2024-12-12 10:57:03 +0700

    add dom for accessing element

---
+++ dom.js
@@ -0,0 +1,31 @@
+const amount = document.getElementById("amount");
+
+console.log(amount);
+
+const steps = document.getElementsByClassName("step");
+
+// console.log(steps);
+
+const circles = document.getElementsByClassName("circle");
+
+// console.log(circles);
+
+const element = document.querySelector(".receiver > div > div");
+
+console.log(element);
+
+const elements = document.querySelectorAll("form > div");
+
+console.log(elements);
+
+// const a = document.getElementsByTagName("a")
+
+const receivers = document.getElementsByClassName("receiver");
+
+console.log(receivers);
+
+const receiverImg = receivers[0].querySelectorAll("img");
+const allImg = document.querySelectorAll("img");
+
+console.log(receiverImg);
+console.log(allImg);
